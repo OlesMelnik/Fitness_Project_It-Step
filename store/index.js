bf1c2de08c2035e63fdc624b1963ecee1cf67476
Valeriy <enjay240@yandex.ua>
2020-12-16 19:20:16 +0200

    store

---
+++ store/index.js
@@ -0,0 +1,27 @@
+import authGuard from "../middleware/authGuard"
+
+export const state = () => ({
+    auth: {
+        uid: null,
+    },
+})
+
+
+export const getters = {
+    authenticated(state) {
+        return state.auth.uid;
+    },
+}
+
+export const mutations = {
+    auth(state, {uid}) {
+        state.auth.uid = uid;
+
+        try {
+            this.$cookies.set('uid', uid, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 7
+            })
+        } catch(ex) {}
+    },
+}
