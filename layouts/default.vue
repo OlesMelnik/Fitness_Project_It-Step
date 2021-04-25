8cb61158f624aed84fd87970cd20f6e261907f30
Valeriy Tkachuk <enjaychannle@gmail.com>
2021-04-25 20:00:39 +0300

    new-header
    new-header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,54 @@
 <template>
 	<div class="ms">
-		<nav class="navbar header has-shadow is-primary" role="navigation" aria-label="main navigation">
-		<div class="navbar-brand">
-			<a class="navbar-item" href="/">
-				<lottie :width="60" :options="lottieOptions" v-on:animCreated="handleAnimation" />
-			
-				<div class="logo">VueFitness</div>
-			</a>
+		<b-navbar>
+			<template #brand>
+				<b-navbar-item tag="router-link" :to="{ path: '/' }">
+					<lottie :width="60" :options="lottieOptions" v-on:animCreated="handleAnimation" />
+				
+					<div class="logo">VueFitness</div>
+				</b-navbar-item>
+			</template>
+			<template #start>
+				<b-navbar-item href="#">
+					Home
+				</b-navbar-item>
+				<b-navbar-item href="#">
+					Documentation
+				</b-navbar-item>
+				<!-- <b-navbar-dropdown label="Info">
+					<b-navbar-item href="#">
+						About
+					</b-navbar-item>
+					<b-navbar-item href="#">
+						Contact
+					</b-navbar-item>
+				</b-navbar-dropdown> -->
+			</template>
 
-			<div class="navbar-burger">
-			<span />
-			<span />
-			<span />
-			</div>
-		</div>
-		</nav>
+			<template #end>
+				<b-navbar-item tag="div">
+					<div class="user">
+						<div class="avatar">
+							<img src="/assets/img/user.png" :alt="userInfo.login">
+						</div>
+						<div class="info">
+							<div class="login">
+								{{userInfo.login}}
+							</div>
+							<div class="balance">
+								<b>Баланс: </b>{{userInfo.balance}}
+								
+            					<nuxt-link to="/cabinet/payment">
+									<b-icon icon="plus-box"></b-icon>
+								</nuxt-link>
+							</div>
+						</div>
+					</div>
+				</b-navbar-item>
+			</template>
+    	</b-navbar>
+
+		
 
 		<section class="main-content columns">
 		<aside class="column is-2 section">
@@ -77,6 +111,10 @@
 		},
 		data () {
 			return {
+				userInfo: {
+					login: 'EnJay',
+					balance: 100,
+				},
 				anim: null,
 				lottieOptions: { animationData: animationData.default }
 			}
@@ -89,15 +127,53 @@
 	}
 </script>
 <style>
-	body, html{
-		background: rgb(12, 1, 26);
+	.navbar-item img{
+		max-height: 100px !important;
 	}
 </style>
 <style scoped>
 	.logo{
 		font-size: 35px;
 		font-weight: bold;
-		color:white;
+		color:rgb(152, 0, 182);
 		font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+	}
+	.user{
+		height: 100%;
+		display: flex;
+	}
+	.user .avatar{
+		width: 60px;
+		height: 60px;
+		border-radius: 50%;
+		background-color: #21D4FD;
+		background-image: linear-gradient(225deg, #21D4FD 0%, #B721FF 100%);
+		transition: 0.2s;
+		text-align: center;
+		box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+	}
+	.user .avatar:hover{
+		transition: 0.2s;
+		transform: rotate(10deg);
+		box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+	}
+	.user .info{
+		margin-left: 15px;
+	}
+	.user .info .login{
+		font-size: 22px;
+		font-weight: 600;
+	}
+	.user .info .balance .icon{
+		cursor: pointer;
+		vertical-align: middle;
+	}
+	.user .info .balance .icon:hover{
+		color: green;
+	}
+	.user img{
+		margin-top: 10px;
+		width: 40px;
+		height: 40px !important;
 	}
 </style>
